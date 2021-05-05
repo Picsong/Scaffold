009056606cb3abf10b67ca49e88d6395ed549fb9
Picsong <312465087@qq.com>
2021-05-05 11:47:21 +0800

    pre-commit5

--- src/wrappers/LoginWrapper.tsx
+++ src/wrappers/LoginWrapper.tsx
@@ -3,8 +3,9 @@ import type { FC } from 'react';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
 import { getStorage, Types } from '@/helpers/storage';
+
 const LoginWrapper: FC = ({ children }) => {
-  const isLogin = getStorage(Types.IsLogin, 'local')
+  const isLogin = getStorage(Types.IsLogin, 'local');
 
   if (isLogin) {
     return <ConfigProvider locale={zhCN}>{children}</ConfigProvider>;
