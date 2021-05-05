3bfadcdd77dc550e97db77c5a04f0208c3a169f3
Picsong <312465087@qq.com>
2021-05-05 10:13:49 +0800

    pre-commit2

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,17 @@
 import { history } from 'umi';
-
 export { request } from '@/services';
-
 export async function getInitialState() {
   return {
     auth: {
       page1: true,
-      page2: false,
+      page2: false
     },
   };
 }
 
 export function onRouteChange() {
   // 设置路由变化时当前页面滚动条为0
-  document.documentElement.scrollTop = 0;
+  document.documentElement.scrollTop = 0
 }
 
 export const layout = () => {
